cd30bf3bf1d5a43c6e85eeeab88577586b16643f
Fernanda Dias <fernandasd26@gmail.com>
2021-05-08 12:30:12 -0300

    working with gridview

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:PopcornMovie/presentation/bloc/show_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'data/models/show.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -63,18 +65,71 @@ class _BodyState extends State<Body> {
       body: BlocBuilder<ShowBloc, ShowState>(
         builder: (context, state) {
           if (state is IndexLoadedState) {
-            print('index loaded with success');
+            return Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: GridViewShowList(
+                showList: state.show.showList,
+              ),
+            );
           }
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Fernanda'),
+                Text('Nothing to show here'),
               ],
             ),
           );
         },
       ),
     );
+  }
+}
+
+class GridViewShowList extends StatelessWidget {
+  final List<ShowModel> showList;
+  const GridViewShowList({
+    Key key,
+    @required this.showList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 0.8,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemCount: showList.length,
+        itemBuilder: (BuildContext ctx, index) {
+          return Container(
+            // color: Colors.amber,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  showList[index].name,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  // color: Colors.blue,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 2.0, 0, 0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(
+                        showList[index].image,
+                        fit: BoxFit.fitWidth,
+                        height: 200,
+                        width: 145,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
